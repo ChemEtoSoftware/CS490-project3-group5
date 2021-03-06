83709788e545260c87d1cca21bcf8888c09d5383
ChemEtoSoftware <kevinpereira79@gmail.com>
2021-03-06 15:19:48 +0000

    Made a few changes here

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
 
 app = Flask(__name__, static_folder='./build/static')
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+import models
+db.create_all()
+
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -14,8 +28,7 @@ socketio = SocketIO(
     manage_session=False
 )
 
-users=[]
-previous_arr=["","","","","","","","",""]
+previous_arr = ["", "", "", "", "", "", "", "", ""]
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
@@ -25,16 +38,20 @@ def index(filename):
 @socketio.on('connect')
 def on_connect():
     print('User connected!')
-    
 @socketio.on('disconnect')
 def on_disconnect():
     print('User disconnected!')
 
 @socketio.on('login')
 def on_login(data):
-    global users
-    users.append(data)
-    print("These are the users",users)
+    new_user = models.Person(username=data['user'])
+    db.session.add(new_user)
+    db.session.commit()
+    all_people = models.Person.query.all()
+    users = []
+    for person in all_people:
+        users.append(person.username)
+    print("These are the users", users)
     socketio.emit('login', users, broadcast=True, include_self=True)
 
 @socketio.on('logout')
@@ -49,30 +66,31 @@ def on_logout(data):
 @socketio.on('tictactoe')
 def on_tictactoe(data): # data is whatever arg you pass in your emit call on client
     global previous_arr
-    if(data['message']==["","","","","","","","",""]):
-        previous_arr=data['message'];
-        socketio.emit('tictactoe',  data, broadcast=True, include_self=False)
+    if(data['message'] == ["", "", "", "", "", "", "", "", ""]):
+        previous_arr = data['message']
+        socketio.emit('tictactoe', data, broadcast=True, include_self=False)
         return
-    sum1 = 0;
-    sum2 = 0;
+    sum1 = 0
+    sum2 = 0
     print(str(data['message']))
-    
-    for i in range (0,9):
-        if(previous_arr[i]=='X' or previous_arr[i]=='O'):
-            sum1 += 1;
-        if(data['message'][i]=='X' or data['message'][i]=='O'):
-            sum2 +=1
+    for i in range(0, 9):
+        if(previous_arr[i] == 'X' or previous_arr[i] == 'O'):
+            sum1 += 1
+        if(data['message'][i] == 'X' or data['message'][i] == 'O'):
+            sum2 += 1
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    if(sum1>sum2):
-        socketio.emit('tictactoe',  {'message' : previous_arr}, broadcast=True, include_self=False)
+    if(sum1 > sum2):
+        socketio.emit('tictactoe', {'message' : previous_arr}, broadcast=True, include_self=False)
     else:
-        socketio.emit('tictactoe',  data, broadcast=True, include_self=False)
-        previous_arr=data['message']
+        socketio.emit('tictactoe', data, broadcast=True, include_self=False)
+        previous_arr = data['message']
 
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-    debug=True
-)
+# Note we need to add this line so we can import app in the python shell
+if __name__ == "__main__":
+# Note that we don't call app.run anymore. We call socketio.run with app arg
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    )
