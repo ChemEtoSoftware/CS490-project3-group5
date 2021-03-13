41ed2ad8a0100d975e13c18bfa78133b0728c198
ChemEtoSoftware <kevinpereira79@gmail.com>
2021-03-13 21:17:43 +0000

    I had a lot of problems but I finally managed to resolve them.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import React from './node_modules/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
