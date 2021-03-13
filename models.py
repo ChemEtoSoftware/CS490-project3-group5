6ba9056245756172e21f1753e93d163e697e632a
ChemEtoSoftware <kevinpereira79@gmail.com>
2021-03-13 22:44:54 +0000

    Forgot to change models.py

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
-from app import db
+"""Creates person to insert into database"""
+from app import DB
 
 
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    score = db.Column(db.Integer, unique=False, nullable=False)
-    letter = db.Column(db.String(1), unique=False, nullable=True)
+class Person(DB.Model):
+    """Template for person"""
+    id = DB.Column(DB.Integer, primary_key=True)
+    username = DB.Column(DB.String(80), unique=True, nullable=False)
+    score = DB.Column(DB.Integer, unique=False, nullable=False)
+    letter = DB.Column(DB.String(1), unique=False, nullable=True)
 
     def __repr__(self):
         return '<Person %r>' % self.username
+        
