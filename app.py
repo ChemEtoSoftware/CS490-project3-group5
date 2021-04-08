8a3f0c57d48f9481c4a005f16b972e949fc4e738
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-04-08 14:50:49 +0000

    Added TicketMaster API authorization, keyword search input, server-client communication routes

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
     as well as allowing them the chance to play each other.
 """
 import os
-from flask import Flask, send_from_directory, json
+from flask import Flask, json, request, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 from engineio.payload import Payload
+import requests
 #from helpers import ordered_append, sum_of_arrays, add_to_db
 
 #Prevents server overload
@@ -17,6 +18,7 @@ Payload.max_decode_packets = 200
 load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
 APP = Flask(__name__, static_folder='./build/static')
+APP.secret_key = os.urandom(24)
 # Point SQLAlchemy to your Heroku database
 APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
@@ -39,18 +41,43 @@ SOCKETIO = SocketIO(APP,
 PREVIOUS_ARR = ["", "", "", "", "", "", "", "", ""]
 LIST_OF_ACTIVE_USERS = []
 
+
+'''
 @APP.route('/', defaults={"filename": "index.html"})
 @APP.route('/<path:filename>')
 def index(filename):
     """Fetches the file. Hooks server up to client"""
     return send_from_directory('./build', filename)
+'''
 
+SEARCH_URL = "https://app.ticketmaster.com/discovery/v2/events.json"
+APIKEY = os.getenv("APIKEY")
+@APP.route('/api/post', methods=['POST'])
+def api_post():
+    '''keyword read sent from the front-end'''
+    login_json = request.get_json()
+    keyword = login_json.get('keyword')
+    session["keyword"] = keyword
+    #print(keyword)
+    return keyword
 
 @SOCKETIO.on('connect')
 def on_connect():
     """Simply shows who's connected. Nothing more"""
     print('User connected!')
 
+@APP.route('/api', methods=['GET'])
+def api():
+    '''to send query request to TicketMaster API'''
+    keyword = session.get("keyword", None)
+    print(keyword)
+    redurl = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword={}&apikey={}'.format(
+        session.get("keyword", None), APIKEY)
+    req = requests.get(redurl)
+    #data = json.loads(req.text)
+    jsontext = req.json()
+
+    return jsontext
 
 @SOCKETIO.on('disconnect')
 def on_disconnect():
