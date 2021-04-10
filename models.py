27effdd2a3de618ba61ae2fbd57147715e804506
ChemEtoSoftware <kevinpereira79@gmail.com>
2021-04-10 14:41:11 +0000

    Kevin Pereira

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-"""Class for person template"""
+"""Class for user and bookmark template"""
 from app import DB
 
 class Bookmark(DB.Model):
-    """Can create a Person with username and score. Letter won't be used"""
+    """Can create a Bookmark with username and event_id"""
     id = DB.Column(DB.ForeignKey('user.id'), DB.String(80), primary_key=True, nullable=False)
     event_id = DB.Column(DB.String(80), unique=False, nullable=False)
 
