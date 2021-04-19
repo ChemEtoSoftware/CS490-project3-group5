a66e9586adc708c8f26ee35bb3dfc3f721a0c6eb
Xavier Vargas <Xnv2@njit.edu>
2021-04-19 12:51:07 -0400

    fixing proptype declarations and adding imports

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -1,8 +1,22 @@
 /* eslint-disable */
 import { List, Header } from 'semantic-ui-react';
-export function Bookmarks(eventID, clientId, socket) {
+import PropTypes from 'prop-types';
+import io from 'socket.io-client';
+
+export function Bookmarks(props) {
+  const { eventID, clientId, socket } = props;
   alert(eventID);
   alert(clientId);
   socket.emit('create_bookmark', {id : clientId, eventID : eventID})
 }
+Bookmarks.propTypes = {
+  eventID: PropTypes.string,
+  clientId: PropTypes.string,
+  socket: PropTypees.instanceOf(io()),
+};
+Bookmarks.defaultProps = {
+  eventID: '',
+  clientId: '',
+  socket: null,
+};
 export default Bookmarks;
