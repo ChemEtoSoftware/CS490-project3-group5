73b5462b8013e465ce4e50158832f0d676f4b021
Xavier Vargas <xnv2@njit.edu>
2021-04-29 07:47:09 +0000

    Merging comments into main

--- src/EventContainer.js
+++ src/EventContainer.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-underscore-dangle */
+
 import React, { useState, useEffect } from 'react';
+import {
+  MapContainer, TileLayer, Marker, Popup,
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 // import io from 'socket.io-client';
+import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
 import './App.css';
 import PropTypes from 'prop-types';
 import { List, Header } from 'semantic-ui-react';
 import { Comment } from './Comment';
-import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
-import {
-  MapContainer, TileLayer, Marker, Popup,
-}
-  from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 
 export function EventPage(props) {
   /* Couldn't figure out how to pass variables as props in onclick,
@@ -93,75 +93,75 @@ export function EventPage(props) {
   };
   return (
     <div>
-    <div>
-      <List key={clientId * i}>
-        <div className="container" key={clientId + i}>
-          <List.Item key={currEvent.id + n}>
-            <Header key={currEvent.id + n1}>{currEvent.name}</Header>
-            <img key={currEvent.id + n2} src={currEvent.images[0].url} alt="" width="300" height="200" />
-            <div key={currEvent.id + n3} className="buttonHolder">
-              <button type="button" className="search" onClick={Bookmarks}> Bookmark </button>
-            </div>
-            <p key={currEvent.id + n4}>{currEvent.dates.start.localDate}</p>
-            <div className="like-dislike">
-              <h3>
-                <button
-                  label="likeButton"
-                  type="button"
-                  onClick={() => {
-                    console.log(currEvent.id);
-                    // socket.emit('dislikeEvent', { eventID: currEvent.id, isLiked: false });
-                    updateLikes();
-                  }}
-                >
-                  <FaThumbsUp />
-                </button>
-                {likes}
-                <span> </span>
-                likes
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <button
-                  label="dislikeButton"
-                  type="button"
-                  onClick={() => {
-                    console.log(currEvent.id);
-                    // socket.emit('dislikeEvent', { eventID: currEvent.id, isLiked: false });
-                    updateDislikes();
-                  }}
-                >
-                  <FaThumbsDown />
-                </button>
-                {dislikes}
-                <span> </span>
-                dislikes
-              </h3>
-            </div>
-          </List.Item>
-        </div>
-      </List>
-      <Comment clientId={clientId} eventId={currEvent.id} socket={socket} />
-    </div>
-    <div>
-      <div className="mapid">
-        <MapContainer center={[lat, long]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker key={`marker-${currEvent.id}`} position={[lat, long]}>
-            <Popup>
-              {currEvent.name}
-              <br />
-              {typeof currEvent._embedded.venues[0].address === 'undefined' ? null : currEvent._embedded.venues[0].address.line1}
-              ,
-              {typeof currEvent._embedded.venues[0].city === 'undefined' ? null : currEvent._embedded.venues[0].city.name}
-              ,
-              {typeof currEvent._embedded.venues[0].state === 'undefined' ? null : currEvent._embedded.venues[0].state.stateCode}
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <div>
+        <List key={clientId * i}>
+          <div className="container" key={clientId + i}>
+            <List.Item key={currEvent.id + n}>
+              <Header key={currEvent.id + n1}>{currEvent.name}</Header>
+              <img key={currEvent.id + n2} src={currEvent.images[0].url} alt="" width="300" height="200" />
+              <div key={currEvent.id + n3} className="buttonHolder">
+                <button type="button" className="search" onClick={Bookmarks}> Bookmark </button>
+              </div>
+              <p key={currEvent.id + n4}>{currEvent.dates.start.localDate}</p>
+              <div className="like-dislike">
+                <h3>
+                  <button
+                    label="likeButton"
+                    type="button"
+                    onClick={() => {
+                      console.log(currEvent.id);
+                      // socket.emit('dislikeEvent', { eventID: currEvent.id, isLiked: false });
+                      updateLikes();
+                    }}
+                  >
+                    <FaThumbsUp />
+                  </button>
+                  {likes}
+                  <span> </span>
+                  likes
+                  <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                  <button
+                    label="dislikeButton"
+                    type="button"
+                    onClick={() => {
+                      console.log(currEvent.id);
+                      // socket.emit('dislikeEvent', { eventID: currEvent.id, isLiked: false });
+                      updateDislikes();
+                    }}
+                  >
+                    <FaThumbsDown />
+                  </button>
+                  {dislikes}
+                  <span> </span>
+                  dislikes
+                </h3>
+              </div>
+            </List.Item>
+          </div>
+        </List>
+        <Comment clientId={clientId} eventId={currEvent.id} socket={socket} />
       </div>
-    </div>
+      <div>
+        <div className="mapid">
+          <MapContainer center={[lat, long]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker key={`marker-${currEvent.id}`} position={[lat, long]}>
+              <Popup>
+                {currEvent.name}
+                <br />
+                {typeof currEvent._embedded.venues[0].address === 'undefined' ? null : currEvent._embedded.venues[0].address.line1}
+                ,
+                {typeof currEvent._embedded.venues[0].city === 'undefined' ? null : currEvent._embedded.venues[0].city.name}
+                ,
+                {typeof currEvent._embedded.venues[0].state === 'undefined' ? null : currEvent._embedded.venues[0].state.stateCode}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </div>
   );
 }
