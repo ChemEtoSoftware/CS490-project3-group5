0e90a5f7976d2ec74bba652b829cfda1f8163c94
Ivana Zdravevska <ec2-user@ip-172-31-2-160.ec2.internal>
2021-04-29 02:41:51 +0000

    After pylint

--- models.py
+++ models.py
@@ -26,17 +26,10 @@ def get_bookmarks(d_b):
         def __repr__(self):
             return '<Bookmark %r>' % self.id
     return Bookmarks
-'''    
-class LikesDislikes(DB.Model):
-    eventID = DB.Column(DB.String(80), primary_key=True)
-    likes = DB.Column(DB.Integer, nullable=False)
-    dislikes = DB.Column(DB.Integer, nullable=False)
-    def __repr__(self):
-        return '<LikesDislikes %r>' % self.eventID
-'''
-
 def get_likes_dislikes(d_b):
+    '''Model for Likes and Dislikes Feature'''
     class LikesDislikes(d_b.Model):
+        '''Can create DB with eventID, num of likes and dislikes'''
         eventID = d_b.Column(d_b.String(80), primary_key=True)
         likes = d_b.Column(d_b.Integer, nullable=False)
         dislikes = d_b.Column(d_b.Integer, nullable=False)
